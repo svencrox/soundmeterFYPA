5d1b3b352a7cefbb324d650c9f95e4f2a7c6cd7b
Wilson Ting <wilsontingch@yahoo.com>
2019-04-15 14:29:01 +0800

    working code

--- sound_meter_FYP/py_test2.py
+++ sound_meter_FYP/py_test2.py
@@ -6,14 +6,15 @@ import numpy
 import pika
 import time
 from time import sleep
-#import sys
+import sys
 import random
 import cloud4rpi
 # start a connection with localhost
-DEVICE_TOKEN = '419CxuErk7dkg1oeKGX2bV783'
-DATA_SENDING_INTERVAL = 10  # secs
-DIAG_SENDING_INTERVAL = 15 # secs
-POLL_INTERVAL = 1.0  # 500 ms
+DEVICE_TOKEN = '3gqBRMsVqFw6jQuU4twwjkKse'
+#DEVICE_TOKEN = '419CxuErk7dkg1oeKGX2bV783'
+DATA_SENDING_INTERVAL = 1 # secs
+DIAG_SENDING_INTERVAL = 2# secs
+POLL_INTERVAL = 0.1  # 500 ms
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 # this is a queue named hello
@@ -21,24 +22,24 @@ channel.queue_declare(queue='hello')
 
 
 
-CHUNKS = [8192, 9600]       # originally for CD quality is 4096 but if it fails then put in more
-CHUNK = CHUNKS[1]
+#CHUNKS = [10000, 30000]       # originally for CD quality is 4096 but if it fails then put in more
+CHUNK = 1024
 FORMAT = pyaudio.paInt16    # 16 bit
 CHANNEL = 1    # 1 for mono. 2 for stereo
 STATUS = "HELLO"
 
-RATES = [44300, 48000] #mic rates
-RATE = RATES[1]
+#RATES = [44300, 48000] #mic rates
+RATE = 44100
 
 NUMERATOR, DENOMINATOR = spl.A_weighting(RATE)
 
-def get_path(base, tail, head=''):
-    return os.path.join(base, tail) if head == '' else get_path(head, get_path(base, tail)[1:])
+#def get_path(base, tail, head=''):
+    #return os.path.join(base, tail) if head == '' else get_path(head, get_path(base, tail)[1:])
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #HTML_PATH = get_path(BASE_DIR, 'html/main.html', 'file:///')
-SINGLE_DECIBEL_FILE_PATH = get_path(BASE_DIR, 'decibel_data/single_decibel.txt')
-MAX_DECIBEL_FILE_PATH = get_path(BASE_DIR, 'decibel_data/max_decibel.txt')
+#SINGLE_DECIBEL_FILE_PATH = get_path(BASE_DIR, 'decibel_data/single_decibel.txt')
+#MAX_DECIBEL_FILE_PATH = get_path(BASE_DIR, 'decibel_data/max_decibel.txt')
 
 # MIC LISTENING
 pa = pyaudio.PyAudio()
@@ -68,17 +69,30 @@ def click(id):
 # def open_html(path):
 #     driver.get(path)
 
-def update_max_if_new_is_larger_than_max(new, max):
-    print("update_max_if_new_is_larger_than_max called")
-    if new > max:
-        print("max observed")
-        update_text(MAX_DECIBEL_FILE_PATH, 'MAX: {:.2f} dBA'.format(new))
-        click('update_max_decibel')
-        return new
+#def update_max_if_new_is_larger_than_max(new, max):
+    #print("update_max_if_new_is_larger_than_max called")
+    #if new > max:
+       # print("max observed")
+        #update_text(MAX_DECIBEL_FILE_PATH, 'MAX: {:.2f} dBA'.format(new))
+        #click('update_max_decibel')
+       # return new
+    #else:
+        #return max
+def status():
+    
+    new_decibel = listen_for_events()
+    print('Decibel: {:+.2f} dB'.format(new_decibel))
+    # print status according to decibel output
+    if (new_decibel < 40):
+        STATUS = "LOW"
+    elif(new_decibel<=70 ):
+        STATUS = "MEDIUM"
     else:
-        return max
+        STATUS = "HIGH"
+    return STATUS
 
 def listen_for_events():
+    
     old=0
     error_count=0
     min_decibel=100
@@ -87,7 +101,7 @@ def listen_for_events():
     while True:
         try:
             ## read() returns string which needs to convert into array.
-            block = stream.read(CHUNK)
+            block = stream.read(CHUNK, exception_on_overflow = False)
         except IOError as e:
             error_count += 1
             print(" (%d) Error recording: %s" % (error_count, e))
@@ -95,19 +109,10 @@ def listen_for_events():
             decoded_block = numpy.fromstring(block, 'Int16')
             y = lfilter(NUMERATOR, DENOMINATOR, decoded_block)
             new_decibel = 20*numpy.log10(spl.rms_flat(y))
-            if is_meaningful(old, new_decibel):
-                old = new_decibel
-                print('Decibel: {:+.2f} dB'.format(new_decibel))
-                # print status according to decibel output
-                if (new_decibel < 40):
-                    STATUS = "LOW"
-                elif(new_decibel<=70 ):
-                    STATUS = "MEDIUM"
-                else:
-                    STATUS = "HIGH"
-                print(STATUS)
-                return STATUS
-                time.sleep(2)
+            
+            return new_decibel
+                
+            time.sleep(2)
 
             #sending hello world with routing key which refers to queue
             # channel.basic_publish(exchange='',
@@ -159,11 +164,16 @@ def listen_for_events():
 
 def listen():
     variables = {
-       
-        'STATUS': {
-            'type': 'string',
+        'Sound': {
+            'type': 'numeric',
             'bind': listen_for_events
+             
+        },
+        'Status': {
+            'type': 'string',
+            'bind': status
         }
+        
     }
 
     
@@ -184,7 +194,7 @@ def listen():
         device.publish_config()
 
         # Adds a 1 second delay to ensure device variables are created
-        sleep(1)
+        #sleep(1)
 
         data_timer = 0
         diag_timer = 0
@@ -215,3 +225,4 @@ def listen():
 
 if __name__ == '__main__':
     listen()
+    
